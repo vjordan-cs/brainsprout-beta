cc0d661123a8e3a39fefb6f1934f5edd62ef9bf5
vjordan-cs <vjordan.cs@gmail.com>
2023-10-04 14:11:14 -0500

    clean slate

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function About() {
   return (
     <div className="bg-slate-600 h-36">
       <div className="text-center text-white flex items-center justify-center h-36">
-        Brainsprout
+        About
       </div>
-      <div className="border bg-slate-100 p-2 w-min">
-        <Link href="/about">about</Link>
-      </div>
+      <Link href="/">home</Link>
     </div>
   );
 }
