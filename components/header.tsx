056b3c7bce98ffdab6030d82674ec25853b51d0c
vjordan-cs <vjordan.cs@gmail.com>
2023-10-19 10:47:27 -0500

    add constants and component folders

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-slate-600 h-36">
+      <div className="text-center text-white flex items-center justify-center h-36">
+        Welcome to Brainsprout!
+      </div>
+    </div>
+  );
+}
