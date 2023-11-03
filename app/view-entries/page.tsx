52d76d2cb4f15f71e72cdeddb6da52cba8b334e0
vjordan-cs <vjordan.cs@gmail.com>
2023-11-03 13:13:23 -0500

    structure for header and entries

---
+++ app/view-entries/page.tsx
@@ -0,0 +1,13 @@
+export default function ViewEntries() {
+  return (
+    <div className="static">
+      <div className="absolute top-5 left-5 bg-white p-4">back</div>
+      <div className="h-screen">
+        <div className="bg-red-200 h-1/4"></div>
+        <div className="h-3/4">
+          <div className="bg-blue-100 h-1/6"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
