e4195f06a17326511d4895bb6314592ee550b19d
vjordan-cs <vjordan.cs@gmail.com>
2023-11-19 22:53:44 -0600

    landing page greyblocked

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-slate-600 h-36">
-      <div className="text-center text-white flex items-center justify-center h-36">
-        Brainsprout
+    <div className="h-screen md:flex">
+      <div className="h-2/3 bg-slate-500 flex justify-center items-center md:h-screen md:w-2/3">
+        <div className="text-center">
+          <div className="text-5xl md:text-6xl">☘️ BrainSprout</div>
+          <div className="p-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            exercitationem dolor consequuntur? Atque corrupti, illo magni
+            aliquid maxime animi omnis vero illum soluta dolore officiis
+            doloremque excepturi quae culpa velit!
+          </div>
+        </div>
       </div>
-      <div className="border bg-slate-100 p-2 w-min">
-        <Link href="/about">about</Link>
+      <div className="h-1/3 bg-red-200 flex justify-center items-center md:h-screen md:w-1/3">
+        <div className="text-center">
+          <div className="text-xl mb-10">Sign In</div>
+          <Link
+            href=""
+            className="border-2 border-slate-100 bg-slate-100 rounded-lg p-6"
+          >
+            Sign In Button
+          </Link>
+        </div>
       </div>
     </div>
   );
