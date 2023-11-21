5329ae2a97fa2e1a1b7d104cd1ed9f87ddf4c366
vjordan-cs <vjordan.cs@gmail.com>
2023-11-20 23:41:39 -0600

    new entry edit/save state change

--- app/new-entry/page.tsx
+++ app/new-entry/page.tsx
@@ -6,8 +6,11 @@ import Link from "next/link";
 export default function NewEntry() {
   return (
     <div className="h-screen bg-slate-400 relative">
-      <div className="h-1/6 flex items-center justify-center text-4xl">
-        Journal Prompt
+      <div className="h-1/6 flex items-center justify-center text-2xl px-14 text-center">
+        <div>
+          <div>Journal Prompt</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </div>
       </div>
       <div className="h-5/6 flex justify-center">
         <div className="h-5/6 w-5/6">
--- components/JournalEntryInput.tsx
+++ components/JournalEntryInput.tsx
@@ -1,29 +1,63 @@
+"use client";
+import { SetStateAction, useState, ChangeEvent } from "react";
+
 export default function JournalEntryInput() {
+  const [isEditMode, setIsEditMode] = useState(true);
+  const [text, setText] = useState("");
+
+  const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+  };
+
+  const handleSave = () => {
+    setIsEditMode(false);
+  };
+
+  const handleEdit = () => {
+    setIsEditMode(true);
+  };
   return (
     <div className="h-full relative">
-      <label
-        htmlFor="comment"
-        className="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Add your thoughts below!
-      </label>
-      <div className="mt-2 h-full">
-        <textarea
-          rows={4}
-          name="comment"
-          id="comment"
-          className="block w-full h-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          defaultValue={""}
-        />
-      </div>
-      <div className="absolute -bottom-4 right-6">
-        <button
-          type="submit"
-          className=" inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Save Entry
-        </button>
-      </div>
+      {isEditMode ? (
+        <div className="h-full relative">
+          <label
+            htmlFor="comment"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            November 20th, 2023
+          </label>
+          <div className="mt-2 h-full">
+            <textarea
+              value={text}
+              onChange={handleTextChange}
+              rows={4}
+              name="comment"
+              id="comment"
+              className="block w-full h-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="absolute -bottom-4 right-6">
+            <button
+              onClick={handleSave}
+              type="submit"
+              className=" inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save Entry
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div>{text}</div>
+          <button
+            onClick={handleEdit}
+            type="submit"
+            className=" inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Edit Entry
+          </button>
+        </div>
+      )}
     </div>
   );
 }
